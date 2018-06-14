9c6adc1cdf90ff478818a54a7eb3f5fe6551b31f
Duncan Falconer <duncan.niamh@gmail.com>
2018-06-14 20:14:32 +0000

    Made a assert test-rig

---
+++ byotest.py
@@ -0,0 +1,13 @@
+
+def test_are_equal(actual, expected):
+    assert expected == actual, "Expected {0}, got {1}".format(expected,actual)
+ 
+def test_not_equal(a, b):
+    assert a != b, "Did not expect {0}, got {1}".format(a, b)
+    
+def test_is_in(collection, item):
+    assert item in collection, "{0} does not contain {1}".format(collection, item)
+  
+def test_between(a, b):
+    assert a <=4 and b >=6, "Expecting {0}, got {1}".format(a, b)
+
