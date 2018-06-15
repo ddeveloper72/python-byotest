86b70262f7e052d8cab7500562577d89e101af79
Duncan Falconer <duncan.niamh@gmail.com>
2018-06-15 10:33:26 +0100

    completed the vending machine tutorial

--- byotest.py
+++ byotest.py
@@ -1,13 +1,17 @@
+"""
+def array_of_numbers(numbers):
+    return 0
+"""
 
 def test_are_equal(actual, expected):
-    assert expected == actual, "Expected {0}, got {1}".format(expected,actual)
- 
+    assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
+
+
 def test_not_equal(a, b):
-    assert a != b, "Did not expect {0}, got {1}".format(a, b)
-    
+    assert a != b, "Did not expect {0}, but got {1}".format(a, b)
+
+
 def test_is_in(collection, item):
     assert item in collection, "{0} does not contain {1}".format(collection, item)
-  
-def test_between(a, b):
-    assert a <=4 and b >=6, "Expecting {0}, got {1}".format(a, b)
 
+#test_between(array_of_numbers([1,2,3,4,5,6]), 5)
